6de2ece3241b4e7afda466de051d2cf5cd097755
mohsen <mohsen.gharivand@gmail.com>
2023-08-28 15:08:28 +0330

    Detect device language

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,6 +1,13 @@
 import i18n from 'i18next';
+import {NativeModules, Platform} from 'react-native';
 import {initReactI18next} from 'react-i18next';
 import * as resources from './resources';
+
+const deviceLanguage =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0]
+    : NativeModules.I18nManager.localeIdentifier;
 
 const ns = Object.keys(Object.values(resources)[0]);
 export const defaultNS = ns[0];
@@ -17,7 +24,7 @@ i18n.use(initReactI18next).init({
       {},
     ),
   },
-  lng: 'en',
+  lng: deviceLanguage,
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
